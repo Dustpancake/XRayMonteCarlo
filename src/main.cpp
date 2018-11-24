97bd9d560abc0bef550cf7cb700919ce9e7f20a6
ktulhu <fergusbkr@gmail.com>
2018-11-24 05:45:05 +0000

    added curl call

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@
 #define MAX 1.0
 using namespace std;
 
-int main() {
-	TH1F* h_Uniform = new TH1F("h_Uniform", "uniform random numbers",  100,  0, MAX);
-	std::clock_t start;
+void rngtest(){
+    TH1F* h_Uniform = new TH1F("h_Uniform", "uniform random numbers",  100,  0, MAX);
+    std::clock_t start;
 /*
 	start = std::clock();
 	for (int i = 0; i<100000000; i++) {
@@ -20,13 +20,23 @@ int main() {
 	std::cout << "time taken = " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
 */
 
-	start = std::clock();
+    start = std::clock();
     prng::ncallback(100000000, 0, MAX, [&](double r){ h_Uniform->Fill(r); });
     std::cout << "time taken = " << (std::clock() - start) / (double)(CLOCKS_PER_SEC / 1000) << " ms" << std::endl;
 
-	h_Uniform->SetMinimum(0);
-	TCanvas canvas;
-	h_Uniform->Draw();
-	canvas.Print("output.png");
-	return 0;
+    h_Uniform->SetMinimum(0);
+    TCanvas canvas;
+    h_Uniform->Draw();
+    canvas.Print("output.png");
+}
+
+void test_fetching(){
+    // url http://henke.lbl.gov/tmp/xray7615.dat
+    system("curl http://henke.lbl.gov/tmp/xray7615.dat >> ../xraydata/test.txt");
+}
+
+
+int main() {
+    test_fetching();
+    return 0;
 }
