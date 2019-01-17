709a71e36aaa9583da9d2395228b09ea51abd16b
ktulhu <fergusbkr@gmail.com>
2019-01-17 16:57:42 +0000

    bug fix

--- all_Be.sh
+++ all_Be.sh
@@ -2,9 +2,9 @@
 
 declare -i count=0
 for i in $(find ./xraydata/atten_lengths/Comp -maxdepth 1 -type f \( ! -name . \) -exec echo '{}' \;); do
-    ./build/run simulate 1e7 1 1 100 $i ./xraydata/e_spectra/test.txt
+    ./build/run single 1e7 1 $i ./xraydata/e_spectra/test.txt
     sleep 0.1
-    mv output.png ./composition_out/comp_$count.png
-    mv output.txt ./composition_out/comp_$count.txt
+    mv output.png ./singles/single_$count.png
+    mv output.txt ./singles/single_$count.txt
     count=count+1
 done
